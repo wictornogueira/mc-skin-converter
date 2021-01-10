bbc14d2094eaec2fb9131be9c14de1c1d5ee995a
Wictor Nogueira <wictorgoesnogueira@gmail.com>
2021-01-10 02:05:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Skin Converter
+
+A really simple web application that not only takes a modern Minecraft skin and converts it to the legacy format, but also blits the upper layer onto the lower one, producing more coherent results.
+
+[LIVE DEMO](https://sc.ralphie.xyz/)
---
+++ public/script.js
@@ -0,0 +1,45 @@
+function asyncImage (src) {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.onload = () => resolve(img)
+    img.onerror = reject
+    img.src = src
+  })
+}
+
+function asyncFileReader (file) {
+  return new Promise((resolve, reject) => {
+    const fr = new FileReader()
+    fr.onload = () => resolve(fr)
+    fr.onerror = reject
+    fr.readAsDataURL(file)
+  })
+}
+
+async function convert (input, canvas, result) {
+  const file = input.files[0]
+  if (file.type !== 'image/png') { return }
+
+  const fr = await asyncFileReader(file)
+  const img = await asyncImage(fr.result)
+
+  const ctx = canvas.getContext('2d')
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(img, 0, 0)
+  ctx.drawImage(img, 0, 32, 16, 16, 0, 16, 16, 16)
+  ctx.drawImage(img, 16, 32, 24, 24, 16, 16, 24, 24)
+  ctx.drawImage(img, 40, 32, 16, 16, 40, 16, 16, 16)
+
+  result.src = canvas.toDataURL('image/png')
+}
+
+const fileInput = document.getElementById('file')
+const download = document.getElementById('download')
+const result = document.getElementById('result')
+const canvas = document.createElement('canvas')
+
+canvas.width = 64
+canvas.height = 32
+
+result.onload = () => download.href = result.src
+fileInput.onchange = () => convert(fileInput, canvas, result)
